f3dda0079d96f183e4805768e9e964eb06747008
SkiMsyk <Sakai.Masayuki@keio.jp>
2020-08-06 09:34:02 +0900

    finish DrawingPathsAndShapes

--- DrawingPathsAndShapes/Landmarks/Landmarks/HexagonParameters.swift
+++ DrawingPathsAndShapes/Landmarks/Landmarks/HexagonParameters.swift
@@ -55,3 +55,9 @@ struct HexagonParameters {
         )
     ]
 }
+
+struct HexagonParameters_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
